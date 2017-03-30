fa9b933208b3a5490b0cf8d85efe209de2775eed
weiyueyan <xiaoguaishou0809@gmail.com>
2017-03-30 19:13:07 +0800

    add validation to title

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -9,9 +9,11 @@ class MoviesController < ApplicationController
 
   def create
    @movie = Movie.new(movie_params)
-   @movie.save
-
+   if @movie.save
      redirect_to movies_path
+   else
+     render :new
+ end
  end
 
  def show
