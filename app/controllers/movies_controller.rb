96ecc9e858752f7666081a07e5a77dee56a809b2
weiyueyan <xiaoguaishou0809@gmail.com>
2017-03-31 13:43:00 +0800

    using scope to write order by created_at to group's post

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -21,7 +21,7 @@ before_action :find_movie_and_check_permission, only: [:edit, :update, :destroy]
 
  def show
    @movie = Movie.find(params[:id])
-   @reviews = @movie.reviews
+   @reviews = @movie.reviews.recent
  end
 
  def edit
--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,5 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :movie
   validates :content, presence: true
+  scope :recent, -> { order("created_at DESC")}
 end
